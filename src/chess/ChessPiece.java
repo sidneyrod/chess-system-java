60a85e65f9312f9c4ed3b70cf7da7caac6bac40d
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-02-12 15:01:03 -0300

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -11,11 +11,14 @@ public abstract class ChessPiece extends Piece{
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
-
 	}
 
 	public Color getColor() {
 		return color;
+	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
 	}
 	
 	protected boolean isThereOpponentPiece(Position position) {
