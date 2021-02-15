3580938e6a71a78308670eaf10488826a97661ee
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-02-15 11:03:19 -0300

    Pawn

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -5,7 +5,7 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece{
+public class King extends ChessPiece {
 
 	public King(Board board, Color color) {
 		super(board, color);
